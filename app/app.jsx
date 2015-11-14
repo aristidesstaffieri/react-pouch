c37c96d4ef2184e3a82f8ac6179cdef9dc8625fd
Aristides Staffieri <aristides@Aristidess-MacBook-Pro.local>
2015-11-14 16:19:08 -0700

    updates for build start

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,40 @@
-import './favicon.ico';
-import './index.html';
-import 'babel-core/polyfill';
-import 'normalize.css/normalize.css';
-import './scss/app.scss';
+import './favicon.ico'
+import './index.html'
+import 'babel-core/polyfill'
+import 'normalize.css/normalize.css'
+import './scss/app.scss'
 
-import React from 'react';
-import App from './components/App/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, compose } from 'redux'
+import { Provider } from 'react-redux'
+import { devTools, persistState } from 'redux-devtools'
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react'
+import Main from './containers/App'
+import reducer from './reducers/reducers'
 
-React.render(
-  <App />,
-  document.getElementById('app')
-);
+const finalCreateStore = compose(
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)(createStore)
+
+let store = finalCreateStore(reducer)
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          <Main />
+        </Provider>
+        <DebugPanel top right bottom>
+          <DevTools store={store}
+                    monitor={LogMonitor}
+                    visibleOnLoad={true} />
+        </DebugPanel>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
